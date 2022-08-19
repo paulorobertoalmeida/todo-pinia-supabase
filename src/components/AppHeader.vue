d0aaa02636bc3beee92a73c447d75c04fb858ecd
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-19 17:15:54 +0200

    Creating app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1 class="header-title white">Task</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+  padding: 1rem;
+}
+</style>
