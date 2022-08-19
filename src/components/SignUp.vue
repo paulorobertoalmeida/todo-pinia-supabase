aa315fd68c00412e20f48535280fec88cd500cfd
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-19 09:48:48 +0200

    Siign up update

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -7,22 +7,13 @@
 
 <script setup>
 import PersonalRouter from "./PersonalRouter.vue";
-
 // Route Variables
 const route = "/auth/login";
 const buttonText = "Test the Sign In Route";
-
 // Input Fields
-
 // Error Message
-
 // Show hide password variable
-
 // Show hide confrimPassword variable
-
 // Router to push user once SignedUp to Log In
-
 // Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
 </script>
-
-<style></style>
