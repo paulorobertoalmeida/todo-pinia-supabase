1b76726fa2947106437cddb9452d5324c9f654b1
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-23 11:55:42 +0200

    fixing Sign Up

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,75 +1,157 @@
 <template>
-<!-- component -->
-<!-- page -->
-<main
-  class="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white"
->
   <!-- component -->
-  <section class="flex w-[30rem] flex-col space-y-10" @submit.prevent="signUp">
-    <div class="text-center text-4xl font-medium">Sign Up</div>
-
-    <div
-      class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+  <!-- page -->
+  <main
+    class="
+      mx-auto
+      flex
+      min-h-screen
+      w-full
+      items-center
+      justify-center
+      bg-gray-900
+      text-white
+    "
+  >
+    <!-- component -->
+    <section
+      class="form-sign-in flex w-[30rem] flex-col space-y-10"
+      @submit.prevent="signUp"
     >
-      <input
-        type="text"
-        v-model="email"
-        placeholder="Email or Username"
-        class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-      />
-    </div>
+      <div class="text-center text-4xl font-medium">
+        Sign Up - Register to my App
+      </div>
 
-    <div
-      class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
-    >
-      <input
-        type="password"
-        v-model="password"
-        placeholder="Password"
-        class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-      />
-    </div>
-    <div w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500>
-      <input
-        type="password"
-        v-model="confirmPassword"
-        placeholder="Password"
-        class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-      />
-    </div>
+      <div
+        class="
+          w-full
+          transform
+          border-b-2
+          bg-transparent
+          text-lg
+          duration-300
+          focus-within:border-indigo-500
+        "
+      >
+        <input
+          type="email"
+          v-model="email"
+          required
+          placeholder="example@gmail.com"
+          class="
+            input-field
+            w-full
+            border-none
+            bg-transparent
+            outline-none
+            placeholder:italic
+            focus:outline-none
+          "
+          id="email"
+        />
+      </div>
 
-    <button
-      class="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400"
-    >
-      LOG IN
-    </button>
+      <div
+        class="
+          w-full
+          transform
+          border-b-2
+          bg-transparent
+          text-lg
+          duration-300
+          focus-within:border-indigo-500
+        "
+      >
+        <input
+          type="password"
+          v-model="password"
+          required
+          placeholder="**********"
+          class="
+            input-field
+            w-full
+            border-none
+            bg-transparent
+            outline-none
+            placeholder:italic
+            focus:outline-none
+          "
+        />
+      </div>
+      <div
+        w-full
+        transform
+        border-b-2
+        bg-transparent
+        text-lg
+        duration-300
+        focus-within:border-indigo-500
+      >
+        <input
+          type="password"
+          v-model="confirmPassword"
+          required
+          placeholder="Password"
+          class="
+            w-full
+            border-none
+            bg-transparent
+            outline-none
+            placeholder:italic
+            focus:outline-none
+          "
+        />
+      </div>
 
-    <a
-      href="#"
-      class="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"
-      >FORGOT PASSWORD?</a
-    >
+      <button
+        type="submit"
+        class="
+          transform
+          rounded-sm
+          bg-indigo-600
+          py-2
+          font-bold
+          duration-300
+          hover:bg-indigo-400
+        "
+      >
+        LOG IN
+      </button>
 
-    <p class="text-center text-lg">
-      No account?
       <a
         href="#"
-        class="font-medium text-indigo-500 underline-offset-4 hover:underline"
-        >Create One</a
+        class="
+          transform
+          text-center
+          font-semibold
+          text-gray-500
+          duration-300
+          hover:text-gray-300
+        "
+        >FORGOT PASSWORD?</a
       >
-    </p>
-  </section>
-</main>
 
+      <p>
+        No account?
+        <PersonalRouter
+          :route="route"
+          :buttonText="buttonText"
+          class="sign-up-link"
+        />
+      </p>
+    <div v-show="errorMsg">{{errorMsg}}</div>
 
-  <div>Sign Up</div>
+    </section>
+  </main>
+
+  <!-- <div>Sign Up</div>
   <PersonalRouter :route="route" :buttonText="buttonText" />
-<form @click.prevent="signUp">
-    <input type="text" v-model="email">
-    <input type="password" v-model="password">
-    <input type="password" v-model="confirmPassword">
-    <input class="button" type="submit">
-  </form>
+  <form @click.prevent="signUp">
+    <input type="text" v-model="email" />
+    <input type="password" v-model="password" />
+    <input type="password" v-model="confirmPassword" />
+    <input class="button" type="submit" />
+  </form> -->
 </template>
 
 <script setup>
@@ -123,5 +205,4 @@ const signUp = async () => {
 </script>
 
 <style>
-
 </style>
