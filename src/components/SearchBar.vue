cf928604c5c61e02b68d4ed2d4cd2d772701bbba
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-28 13:39:57 +0200

    cleaning up code: Search bar css 2 - Still some bugs to fix here

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -69,23 +69,7 @@ label{
             pl-3
             pointer-events-none
 }
-.search-input { 
- @apply: bg-gray-50
-            border border-gray-300
-            text-gray-900 text-sm
-            rounded-lg
-            focus:ring-blue-500 focus:border-blue-500
-            block
-            w-80
-            pl-10
-            p-2.5
-            dark:bg-gray-700
-            dark:border-gray-600
-            dark:placeholder-gray-400
-            dark:text-white
-            dark:focus:ring-blue-500
-            dark:focus:border-blue-500
-}
+
 
 </style>
 
